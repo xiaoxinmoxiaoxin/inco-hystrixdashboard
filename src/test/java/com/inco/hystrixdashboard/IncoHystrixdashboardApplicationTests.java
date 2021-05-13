1fffcf5b02f1e632a7e353192a20b3ac1cc5531e
bz <1848239485@qq.com>
2021-05-13 22:42:10 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	xkgl.iml

---
+++ src/test/java/com/inco/hystrixdashboard/IncoHystrixdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inco.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncoHystrixdashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
